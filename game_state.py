2ac3300c8b185b10c3ce064ab15dab36fba4b1dc
byui_student1045 <sco22022@byui.edu>
2025-04-10 15:37:04 -0600

    Updated game_state to better reflect game

--- game_state.py
+++ game_state.py
@@ -6,4 +6,4 @@ class GameState(Enum):
     X_TURN = 0 # This is active on X's turn
     O_TURN = 1 # This is active on O's turn
     GAME_OVER = 2 # This triggers when the game is over
-    GAME_RESTART = 3 # This allows the game to reset
+    GAME_WIN = 3 # This triggers when the game is won
